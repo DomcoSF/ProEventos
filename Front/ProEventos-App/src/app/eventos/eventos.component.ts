640e8c8516a467e380892626e0531b6a3bddbd9d
DomcoSF <felipe.coutinhod@gmail.com>
2021-05-01 17:49:03 -0300

    Seção 5

--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -8,17 +8,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit {
 
-  public eventos: any;
+  public eventos: any = [];
+  public eventosFiltrados: any = [];
+  widthImg = 50;
+  margin = 2 ;
+  mostrarImagem = true;
+  private _filtroLista ='';
+
+  public get filtroLista()  {
+    return this._filtroLista;
+  }
+
+  public set filtroLista(value: string){
+    this._filtroLista = value;
+    this.eventosFiltrados = this.filtroLista ? this.filtrarEventos(this.filtroLista) : this.eventos;
+  }
+
+  filtrarEventos(filtrarPor: string): any {
+    filtrarPor = filtrarPor.toLocaleLowerCase();
+    return this.eventos.filter(
+      ( evento: { tema: string; local: string; }) => evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1 ||
+      evento.local.toLocaleLowerCase().indexOf(filtrarPor) !== -1
+    );
+  }
+
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
     this.getEventos();
   }
 
+  alterarImagem()
+  {
+    this.mostrarImagem = !this.mostrarImagem;
+  }
+
   public getEventos(): void{
     this.http.get('https://localhost:5001/api/eventos').subscribe(
-      response => this.eventos = response,
-      error => console.log(error)
+      response => {
+        this.eventos = response;
+        this.eventosFiltrados = this.eventos;
+      },
+        error => console.log(error)
     );
   }
 }
